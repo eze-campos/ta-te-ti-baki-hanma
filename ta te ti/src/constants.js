ff5637543694f0dcbef38aa3676fad9fe1ffd947
Ezequiel Campos <lecampos2003@gmail.com>
2025-04-27 04:04:11 -0300

    Initial commit: Baki Ta Te Ti game

---
+++ ta te ti/src/constants.js
@@ -0,0 +1,15 @@
+export const TURNS = { // turnos
+  X: '爆',  // Símbolo que representa a Baki (Explosión/Poder)
+  O: '鬼'   // Símbolo que representa a Yujiro (Demonio/Ogro)
+}
+
+export const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
