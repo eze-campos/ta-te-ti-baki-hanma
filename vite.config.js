2370d510fcd9e9df14329beb0b77e842450844d0
ezequiel <lecampos2003@gmail.com>
2025-04-30 21:38:08 -0300

    fix: simplify build configuration for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    sourcemap: false
-  },
-  publicDir: 'public'
 })
